3d44eae1caf6ed0a79af06aebfbf140eb50dc399
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-09-25 16:11:03 -0300

    Layout e funções

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import Calculator from './main/Calculator';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+<div>
+    <h1>Calculadora</h1>
+    <Calculator/>
+</div>, 
+    document.getElementById('root'));
+    registerServiceWorker();
--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -1,11 +1,52 @@
 import React, {Component} from 'react'
 import './Calculator.css'
+import Button from '../components/Button'
+import Display from '../components/Display'
 
 export default class Calculator extends Component{
+
+    constructor(props){
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigt = this.addDigt.bind(this)
+    }
+
+    clearMemory(){
+        console.log('Limpa')
+    }
+
+    setOperation(operation){
+        console.log(operation)
+    }
+
+    addDigt(n){
+        console.log(n)
+    }
+
     render(){
+       
         return(
             <div className="calculator">
-            
+                <Display value={100}/>
+                <Button label="AC" click={this.clearMemory} triple/>
+                <Button label="/" click={this.setOperation} operation/>
+                <Button label="7" click={this.addDigt}/>
+                <Button label="8" click={this.addDigt}/>
+                <Button label="9" click={this.addDigt}/>
+                <Button label="*" click={this.setOperation} operation/>
+                <Button label="4" click={this.addDigt}/>
+                <Button label="5" click={this.addDigt}/>
+                <Button label="6" click={this.addDigt}/>
+                <Button label="-" click={this.setOperation} operation/>
+                <Button label="1" click={this.addDigt}/>
+                <Button label="2" click={this.addDigt}/>
+                <Button label="3" click={this.addDigt}/>
+                <Button label="+" click={this.setOperation} operation/>
+                <Button label="0" click={this.addDigt} double/>
+                <Button label="." click={this.setOperation}/>
+                <Button label="=" click={this.setOperation}/>
+                
             </div>
         )
     }
